a5d41d0378800005a1b4a132999ce4a1e7580c98
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2021-01-27 13:53:15 -0300

    feat: Identifica usuários e tipos de usuários na sessão
    Foram criados dois novos valores nos arrays de usuários cadastrados(), sendo eles 'id' que se refere ao
    id do usuário, e 'usuario_perfil_id' que se refere ao id que demonstra o tipo do perfil daquele usuario
    (administrativo/usuário comum), a partir desses identificadores, quando o usuário é autenticado, a
    cria esses valores na sessão, para que eles possam ser recuperados em outras partes da aplicação.

--- valida_login.php
+++ valida_login.php
@@ -1,23 +1,31 @@
 <?php
     session_start();
 
+    $usuario_id = null;
+    $usuario_perfil_id = null;
     $usuario_autenticado = false;
     $usuarios_app = array(
-        array('email' => 'adm@servicedesk.com', 'senha' => '1234'),
-        array('email' => 'tecnico@servicedesk.com', 'senha' => '1234')
+        array('id' => 1, 'email' => 'adm@servicedesk.com', 'senha' => '1234', 'usuario_perfil_id' => 1),
+        array('id' => 2, 'email' => 'tecnico@servicedesk.com', 'senha' => '1234', 'usuario_perfil_id' => 1)
     );
 
     foreach($usuarios_app as $user) {
         if($_POST['email'] == $user['email'] && $_POST['senha'] == $user['senha']) {
             $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['usuario_perfil_id'];
         }
     }
 
     if($usuario_autenticado) {
         $_SESSION['autenticado'] = 'SIM';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['usuario_perfil_id'] = $usuario_perfil_id;
         header('Location: home.php');
     } else {
         $_SESSION['autenticado'] = 'NAO';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['usuario_perfil_id'] = $usuario_perfil_id;
         header('Location: index.php?login=erro');
     }
 

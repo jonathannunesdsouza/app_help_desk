3e6ec805edc60c88aa26fb84a6622828c8a23126
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2021-01-27 01:31:34 -0300

    style: Adiciona um card de login Adiciona um card que contém o formulário que será utilizado para fazer o login do usuário, o formulário contem 2 campos de texto, sendo um deles de e-mail e outro senha, e um botão submit para fazer o envio dos dados. o formulário envia os dados (action) para o script 'valida.php' pelo método (method) post.

--- index.php
+++ index.php
@@ -19,7 +19,25 @@
       </a>
     </nav>
 
-    
+    <div class="container">
+      <div class="row">
+        <div class="card-login">
+          <div class="card text-white mb-3" style="max-width: 18rem;">
+          <div class="card-header bg-primary">Login</div>
+          <div class="card-body">
+            <form action="valida.php" method="post">
+              <div class="form-group">
+                <input name="email" type="email" class="form-control" placeholder="E-mail">
+              </div>
+              <div class="form-group">
+                <input name="senha" type="password" class="form-control" placeholder="Senha">
+              </div>
+              <button method='post' class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </body>
 </html>

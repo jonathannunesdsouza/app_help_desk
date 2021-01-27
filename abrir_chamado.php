9468499f3c25b6d03e6d4e3513527f956f42b079
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2021-01-27 02:42:55 -0300

    style: Adiciona formulário na página abrir_chamado.php Adiciona um formulário onde os dados serão utilizados para preencher um pedido de chamado, o formulário possui os campos 'Título', 'Categoria' e 'Descrição' e logo abaixo, dois botões, sendo um deles responsável por adicionar o chamado ao sistema e o outro para voltar uma página atrás (home.php).

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -8,9 +8,85 @@
 
     <!-- Font Awesome -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <style>
+      .card-abrir-chamado {
+        padding: 30px 0 0 0;
+        width: 100%;
+        margin: 0 auto;
+      }
+    </style>
     
   </head>
 
   <body>
+
+    <nav class="navbar navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">
+        App Help Desk
+      </a>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-abrir-chamado">
+          <div class="card">
+            <div class="card-header bg-primary text-light">
+              Preencha os campos e clique em + para adicionar um chamado
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  
+                  <form action="registra_chamado.php" method="post">
+                    <div class="form-group">
+                      <label>Título</label>
+                      <input name="titulo" type="text" class="form-control" placeholder="Título">
+                    </div>
+                    
+                    <div class="form-group">
+                      <label>Categoria</label>
+                      <select name="categoria" class="form-control">
+                        <option>Criação Usuário</option>
+                        <option>Impressora</option>
+                        <option>Hardware</option>
+                        <option>Software</option>
+                        <option>Rede</option>
+                      </select>
+                    </div>
+                    
+                    <div class="form-group">
+                      <label>Descrição</label>
+                      <textarea name="descricao" class="form-control" rows="3"></textarea>
+                    </div>
+
+                    <? if(isset($_GET['registro']) && $_GET['registro'] == 'salvo') { ?>
+                      <div class="text-success">
+                        Chamado registrado com sucesso!
+                      </div>
+                    <? } ?>
+
+                    <div class="row mt-4">
+                      <div class="col-6">
+                        <a href="home.php" class="btn btn-lg btn-warning text-light btn-block">
+                          <i class="fas fa-arrow-circle-left" style="font-size:25px"></i>
+                        </a>
+                      </div>
+
+                      <div class="col-6">
+                        <button class="btn btn-lg btn-success btn-block" type="submit">
+                          <i class="fas fa-plus-circle" style="font-size:25px"></i>
+                      </div>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
   </body>
 </html>
